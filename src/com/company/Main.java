a84c2a83cd5e9797494026f4651a1a041c937ca8
Soldour <56756863+Soldour@users.noreply.github.com>
2020-04-15 01:36:12 -0700

    Create Main.java

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+
+package com.company;
+
+
+
+import java.util.Hashtable;
+
+import java.util.Set;
+
+import java.util.logging.Handler;
+
+
+
+public class Main {
+
+    public static void main(String[] args)
+
+    {
+
+
+
+        MyHashTable1 myHashTable1 = new MyHashTable1();
+
+        System.out.println(myHashTable1.isEmpty());
+
+        myHashTable1.add("Solomon","watched them all" );
+
+        System.out.println( myHashTable1.contains("Solomon"));
+
+        myHashTable1.add("MR.Frodo","The ring keeper" );
+
+        myHashTable1.add("Dr.Strange","stone keeper " );
+
+        myHashTable1.add("Spiderman","not Marvel anymore" );
+
+        myHashTable1.add("Spiderman","My be Sony owns it" );
+
+        System.out.println(myHashTable1.size());
+
+        System.out.println(myHashTable1.get("MR.Frodo"));
+
+       MyHashTable1.remove("Dr.55");
+
+        System.out.println(myHashTable1.get("Spiderman"));
+
+        myHashTable1.contains("Spiderman");
+
+
+
+
+
+
+
+
+
+//        System.out.println(MyHashTable1.size());
+
+//      System.out.println(MyHashTable1.isEmpty());
+
+    }
+
+}
